ddf1bff5db2ec465359abca3cf78db546c3ab2e9
Wallace Alves <43219645+wallacepalace@users.noreply.github.com>
2021-10-05 00:47:34 -0300

    Update unlimited attribute

--- API-Update-Groups.ps1
+++ API-Update-Groups.ps1
@@ -41,7 +41,7 @@ if ($Platform.Length -gt 0) {
 }
 
 # Puxa os detalhes das variáveis acima e carregam no payload seguinte
-Get-FalconHost -Detailed -Filter $Filter -OutVariable HostIds | Out-Null
+Get-FalconHost -Detailed -All -Filter $Filter -OutVariable HostIds | Out-Null
 
 # Payload que executa o comando final com as variáveis preload
 Invoke-FalconRTR -Command 'update install' -Arguments $kb -HostId $HostIds.device_id | ConvertTo-Json >> resultado-kbs.json
